1e4a0b2dfdc98c56b6a68e5faa7e3eb17a25dafa
hieuvvth2009018 <73455351+hieuvvth2009018@users.noreply.github.com>
2021-06-21 12:46:19 +0700

    update lap

---
+++ GIẢI BẤT PHƯƠNG TRÌNH/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace GIẢI_BẤT_PHƯƠNG_TRÌNH
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Nhập số A:");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập số B:");
+            float b = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập số C:");
+            float c = float.Parse(Console.ReadLine());
+            float delta = (b/2) * (b/2) - a*c;
+            Console.WriteLine("Phương trình nhập vào là:\n {0}x^2 + {1}x + {2} = 0 ",a,b,c);
+            Console.WriteLine("Delta Phẩy = {0}", delta);
+
+            if(delta < 0)
+            {
+                Console.WriteLine("Phương trình vô nghiêm.");
+            }
+            else if(delta == 0)
+            {
+                float x = (-b / 2) /a;
+                Console.WriteLine("Phương trình có nghiệm kép x1 = x2 = {0}",x);
+
+            }
+            else
+            {
+                double x1 = 0;
+                double x2 = 0;
+                x1 = ((-b / 2) - Math.Sqrt(delta)) / a;
+                x2 = ((-b / 2) + Math.Sqrt(delta)) / a;
+
+                Console.WriteLine("Phương trình có 2 nghiệm phân biệt:");
+                Console.WriteLine("x1 ={0}", x1);
+                Console.WriteLine("x2 ={0}", x2);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+
+        }
---
+++ lap6/Program2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lap6
+{
+    public class Program2
+    {
+        {
+             void Main(string[] args)
+            {
+                Thermostat thermostat = new Thermostat();
+                Heater Heater = new Heater(60);
+                thermostat.OnTemperatureChange +=
+                    Heater.OnTemperatureChanged;
+                Heater heaterCuong = new Heater(100);
+                thermostat.OnTemperatureChange += 
+                    heaterCuong.OnTemperatureChanged;
+
+                Cooler cooler = new Cooler(80);
+                thermostat.OnTemperatureChange += 
+                    cooler.OnTemperatureChanged;
+
+                string temperature;
+                Console.Write("Enter temperature: ");
+                temperature = Console.ReadLine();
+                thermostat.CurrentTemperature = int.Parse(temperature);
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
