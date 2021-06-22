dcb06f6c9a2b0e2693e9deb3933982b509d2e855
hieuvvth2009018 <73455351+hieuvvth2009018@users.noreply.github.com>
2021-06-22 15:11:31 +0700

    update lap

---
+++ lap 7/Employees.cs
@@ -0,0 +1,14 @@
+﻿namespace lap_7
+{
+    public class Employees
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public double Salary { get; set; }
+
+            public override string ToString()
+            {
+                return $"Tên : {Name}, Tuổi : {Age}, Lương : {Salary}";
+            }
+        }
+    } 
---
+++ lap 7/EmployeesManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lap_7
+{
+    public class EmployeesManager
+    {
+         private Dictionary<string, Employees> _Employeess = new Dictionary<string, Employees>();
+        public void Create()
+        {
+            Console.WriteLine("Vui lòng nhập id: ");
+            var id = Console.ReadLine();
+            Console.WriteLine("Vui lòng nhập tên: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Vui lòng nhập tuổi: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vui lòng nhập lương: ");
+            var salary = double.Parse(Console.ReadLine());
+            _Employeess.Add(id,new Employees()
+            {
+                Name = name,
+                Age = age,
+                Salary = salary
+            });
+        }
+        public void Display ()
+        {
+            foreach (var list in _Employeess)
+            {
+                Console.WriteLine($"id: {list.Key},{list.Value}");
+            }
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Vui lòng nhập id: ");
+            var id = Console.ReadLine();
+            if (_Employeess.ContainsKey(id))
+            {
+                Console.WriteLine("Vui lòng sửa tên: ");
+                var name = Console.ReadLine();
+                Console.WriteLine("Vui lòng sửa tuổi: ");
+                var age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Vui lòng sửa lương: ");
+                var salary = double.Parse(Console.ReadLine());
+                _Employeess[id].Name = name;
+                _Employeess[id].Age = age;
+                _Employeess[id].Salary = salary;
+            }
+            else
+            {
+                Console.WriteLine("Nhân viên không tồn tại!");
+            }
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Vui lòng nhập id: ");
+            var id = Console.ReadLine();
+            if (_Employeess.ContainsKey(id))
+            {
+                _Employeess.Remove(id);
+                Console.WriteLine("Xóa thông tin thành công.");
+            }
+            else
+            {
+                Console.WriteLine("Nhân viên không tồn tại!");
+            }
+        }
+    }
+    }
